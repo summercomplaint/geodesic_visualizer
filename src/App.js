e6a4e66c4842e5d60e6698072d192b5aab941a42
summer <lampman4ever@gmail.com>
2025-10-29 19:05:19 -0400

    added color coding :)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { findEndpoints, findCircle} from './mathUtils.mjs';
+import { findEndpoints, findCircle,partialCircle, slideMap, geodToGeod} from './mathUtils.mjs';
 import { complex} from 'mathjs'
 
 
@@ -31,6 +31,78 @@ const HyperbolicDisk = () => {
     return z.abs() < 0.99; // Slightly less than 1 for numerical stability
   };
 
+ const drawPoint =(ctx, x,y,color)=> {
+    ctx.beginPath();
+    ctx.arc(x,y, 6, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+
+  const drawGeod=(ctx,p1,p2,color, partial=false)=>{
+
+    let circledata=0
+    if (partial===false){
+    const [e1,e2] = findEndpoints(p1,p2)
+    circledata = findCircle(e1,e2)
+    }
+    else{
+    circledata = partialCircle(p1,p2)
+    }
+      
+    const c=diskToCanvas(circledata.circCenter)
+      
+    const trueRadius=radius*circledata.radius
+    ctx.beginPath();
+    ctx.arc(c.x,c.y, trueRadius, circledata.phi1, circledata.phi2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+
+  
+  const getTranslates=(coords)=>{
+      const indices=[0,1,4,5]
+      const newCoords=[]
+      for (const coord of coords){
+        const [p1,p2]=coord
+
+        for (let k=0; k<4;k++) {
+          
+          let i=indices[k]
+          
+          const v1=octVertices[i]
+          const v2=octVertices[(i+1)%8]
+          const q1=octVertices[(i+2)%8]
+          const q2=octVertices[(i+3)%8]
+          
+          newCoords.push([geodToGeod(v1,v2,q2,q1,p1),geodToGeod(v1,v2,q2,q1,p2)])
+          newCoords.push([geodToGeod(q2,q1,v1,v2,p1),geodToGeod(q2,q1,v1,v2,p2)])
+
+        }
+      }
+      return newCoords
+  }
+  const basicTranslates=(ctx,p1,p2,color, partial=false,recursive=2)=>{
+
+      let fullCoords=[[p1,p2]]
+      for (let r=0;r<recursive;r++){
+        let newCoords=getTranslates(fullCoords)
+        fullCoords=fullCoords.concat(newCoords)
+      }
+      for (const coord of fullCoords){
+        const [p1,p2] = coord 
+        drawGeod(ctx,p1,p2,color, partial)
+      }
+  }
+
+  const grey="#bbbbbbff"
+  const l = 2**(-1/4)
+  const octVertices= Array.from({length: 8}, (_, i) =>
+    complex({abs: l, arg:  i * 2 * Math.PI / 8}))
+  
   // Draw everything
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -52,62 +124,58 @@ const HyperbolicDisk = () => {
     ctx.stroke();
     
     //draw center point
-    ctx.beginPath();
-    ctx.arc(center.x, center.y, 6, 0, 2 * Math.PI);
-    ctx.fillStyle = "black";
-    ctx.fill();
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 2;
-    ctx.stroke();
+    drawPoint(ctx,center.x,center.y,"black")
+    
+    /*
+    octVertices.forEach((p, i) => {
+      const canvas = diskToCanvas(p);
+      drawPoint(ctx,canvas.x,canvas.y, "#818181ff")
+    });
+    */
+
+    //drawing the octagonal lattice
+    const redLat='#ff5959ff'
+    const yellowLat='#ffdd48ff'
+    const greenLat= '#66ff47ff'
+    const blueLat='#536dffff'
+    const latticeCols=[redLat,yellowLat,redLat,yellowLat,greenLat,blueLat,greenLat,blueLat]
+    for (let k=0; k<8;k++) {
+      const curr=octVertices[k]
+      const next=octVertices[(k+1)%8]
+
+      drawGeod(ctx,curr,next,latticeCols[k],true)
+      basicTranslates(ctx,curr,next,latticeCols[k], true, 3)
+    }
+
 
     // endpoints of geodesics
     if (selectedPoints.length === 2) {
       const [p1,p2]=selectedPoints
   
       const endpoints = findEndpoints(p1,p2)
-      console.log("endpoints", endpoints)
-    
-      endpoints.forEach((p, i) => {
-        const canvas = diskToCanvas(p);
-        ctx.beginPath();
-        ctx.arc(canvas.x, canvas.y, 6, 0, 2 * Math.PI);
-        ctx.fillStyle = i === 0 ? '#a200ffff' : '#1f16a3ff';
-        ctx.fill();
-        ctx.strokeStyle = 'white';
-        ctx.lineWidth = 2;
-        ctx.stroke();
-      });
       const [e1,e2]=endpoints
-      const circledata = findCircle(e1,e2)
-    
-      console.log("center",circledata)
-      const c=diskToCanvas(circledata.circCenter)
+      endpoints.forEach((p, i) => {
+        const endpt=diskToCanvas(p)
+        drawPoint(ctx,endpt.x,endpt.y,'#000000ff')
+
+      });
       
-      const trueRadius=radius*circledata.radius
-      
-      
-        ctx.beginPath();
-        ctx.arc(c.x,c.y, trueRadius, circledata.phi1, circledata.phi2);
-  
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 2;
-        ctx.stroke();
+      drawGeod(ctx,p1,p2,"black",false)
+      //basicTranslates(ctx,p1,p2,false)
 
     }
     
     // Draw points
     selectedPoints.forEach((p, i) => {
       const canvas = diskToCanvas(p);
-      ctx.beginPath();
-      ctx.arc(canvas.x, canvas.y, 6, 0, 2 * Math.PI);
-      ctx.fillStyle = i === 0 ? '#dc2626' : '#16a34a';
-      ctx.fill();
-      ctx.strokeStyle = 'white';
-      ctx.lineWidth = 2;
-      ctx.stroke();
+      drawPoint(ctx,canvas.x,canvas.y, "#0026ff")
     });
     
   }, [selectedPoints]);
+
+
+
+  //event handling v
 
   const handleClick = (e) => {
     const rect = canvasRef.current.getBoundingClientRect();
--- src/mathUtils.mjs
+++ src/mathUtils.mjs
@@ -21,17 +21,16 @@ export const findEndpoints = (z1,z2) => {
  const normedPhiZ2=multiply(phiZ2,complex(1/phiZ2.abs(),0))
  
 
- const e1=phiA(normedPhiZ2,z1.neg())
+ const e2=phiA(normedPhiZ2,z1.neg())
 
 
- const e2=phiA(normedPhiZ2.neg(),z1.neg())
+ const e1=phiA(normedPhiZ2.neg(),z1.neg())
+
+ //e1 will always be on the side z1
 
  return [e1,e2]
 }
 
-export const makepos = (theta) => {
-
-}
 
 export const findCircle =(z1,z2) => {
  // takes two points on the boundary of the disk and draws the geodesic between them
@@ -57,8 +56,46 @@ const midAngle = theta1 - clockwiseAngle / 2;
 const midpt= add(circCenter,complex({abs:radius,arg:midAngle}))
 
 
+
 if (midpt.abs()<1) {
-    console.log("midpt outside")
+    const phi1=theta2
+    const phi2=theta1
+    return {circCenter,radius,phi1,phi2}
+}
+else {
+    const phi1=theta1
+    const phi2=theta2
+    return {circCenter,radius,phi1,phi2}
+}
+}
+
+export const partialCircle =(p1,p2) => {
+ // takes two points draws the geodesic between them
+ const [z1,z2]=findEndpoints(p1,p2)
+ 
+
+ const sum = multiply(add(z1,z2),complex(1/2,0))
+ const diff=multiply(add(z1,z2.neg()),complex(1/2,0))
+ const ratio =diff.abs()/sum.abs()
+
+ const circCenter=multiply(sum,complex(1+ratio*ratio,0))
+
+ const radius = add(circCenter,z1.neg()).abs()
+
+ const theta1=add(p1,circCenter.neg()).arg()
+ const theta2=add(p2,circCenter.neg()).arg()
+
+
+ let clockwiseAngle = theta1 - theta2;
+ if (clockwiseAngle < 0) clockwiseAngle += 2 * Math.PI;
+    
+    // Midpoint angle (clockwise from v1)
+const midAngle = theta1 - clockwiseAngle / 2;
+const midpt= add(circCenter,complex({abs:radius,arg:midAngle}))
+
+
+
+if (midpt.abs()<1) {
     const phi1=theta2
     const phi2=theta1
     return {circCenter,radius,phi1,phi2}
@@ -69,25 +106,50 @@ else {
     return {circCenter,radius,phi1,phi2}
 }
 
-
 }
 
-export const deckMap = (f1,f2,m3,m4,z) => {
+
+export const slideMap = (f1,f2,m3,m4,z) => {
 //fixes f1 and f2, moves m3 to m4
 // wont fix the disk unless m3 and m4 are on the geodesic between f1 and f2. so be careful!
 
 const b = multiply(f1,f2).neg() // z1*z2
-console.log("b",b)
-
 const d = multiply(add(multiply(add(f2,m4.neg()),m3),multiply(add(m3,f2.neg()),f1)),add(m4,m3.neg()).inverse())
-console.log("d",d)
 const a = add(d,add(f1,f2))
-console.log("a",a)
-
 const fz = multiply(add(multiply(a,z),b),add(z,d).inverse())
 return fz
 
 }
 
 
-console.log(complex(0,-1).arg())
+export const genMob=(x1,x2,x3,y1,y2,y3, z)=>{
+//sends x1,x2,x3, to y1,y2,y3 respectively
+//maps to 0,infinity,1 respectively first, and then back, so wont work at exactly x2. whatever
+
+const a=multiply(add(x3,x2.neg()),add(x3,x1.neg()).inverse())
+const d=x2.neg()
+const b=multiply(a.neg(),x1)
+const phiZ=multiply(add(multiply(a,z),b),add(z,d).inverse())
+
+const r=multiply(add(y2,y3.neg()),add(y3,y1.neg()).inverse())
+const p=y2
+const q=multiply(r,y1)
+
+const psiPhiZ=multiply(add(multiply(p,phiZ),q),add(phiZ,r).inverse())
+
+return psiPhiZ
+}
+
+export const geodToGeod=(p1,p2,q1,q2,z)=>{
+    //sends the geodesic through p1,p2 to the geodesic through q1,q2
+    const [b1,b2]=findEndpoints(p1,p2)
+    const [d1,d2]=findEndpoints(q1,q2)
+    return genMob(p1,b1,p2, q1,d1,q2,z)
+}
+
+ const x=complex(0.5,0.5)
+const y=complex(0.2,-0.3)
+const u=complex(-0.1,0.8)
+const v=complex(-0.5,0.2)
+
+console.log(geodToGeod(x,y,u,v,complex(0.2001,-0.3)))
